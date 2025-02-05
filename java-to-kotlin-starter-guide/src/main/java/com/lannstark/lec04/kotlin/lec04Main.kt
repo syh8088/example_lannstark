cdb19201f6e8a9fa5f0136338a953995fead4e2a
syh8088 <syh8088@gmail.com>
2025-02-05 14:47:24 +0900

    4강. 코틀린에서 연산자를 다루는 방법
    4강. 코틀린에서 연산자를 다루는 방법

--- java-to-kotlin-starter-guide/src/main/java/com/lannstark/lec04/kotlin/lec04Main.kt
+++ java-to-kotlin-starter-guide/src/main/java/com/lannstark/lec04/kotlin/lec04Main.kt
@@ -1,8 +1,22 @@
 package com.lannstark.lec04.kotlin
 
+import com.lannstark.lec04.JavaMoney
+
 fun main() {
 
+    val money1 = JavaMoney(2000L)
+    val money2 = JavaMoney(1000L)
 
+    if (money1 > money2) {
+        println("Money1 이 Money2 보다 금액이 큽니다.")
+    }
+
+    val money3 = JavaMoney(2000L)
+    val money4 = money3
+    val money5 = JavaMoney(2000L)
+
+    println(money3 === money4)
+    println(money4 == money5)
 
 
 }
