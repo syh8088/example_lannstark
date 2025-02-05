aea2e05082853ba958dd02f6d4f7adcc5ee0f60f
syh8088 <syh8088@gmail.com>
2025-02-05 15:49:35 +0900

    7강. 코틀린에서 예외를 다루는 방법
    7강. 코틀린에서 예외를 다루는 방법

--- java-to-kotlin-starter-guide/src/main/java/com/lannstark/lec07/kotlin/lec07Main.kt
+++ java-to-kotlin-starter-guide/src/main/java/com/lannstark/lec07/kotlin/lec07Main.kt
@@ -1,8 +1,41 @@
 package com.lannstark.lec07.kotlin
 
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+
 fun main() {
 
+    fun parseIntOrThrow(str: String): Int {
+        try {
+            return str.toInt()
+        }
+        catch (e: NumberFormatException) {
+            throw IllegalArgumentException("주어진 ${str}는 숫자가 아닙니다.")
+        }
+    }
 
+    fun parseIntOrThrow2(str: String): Int? {
+        return try {
+            str.toInt()
+        } catch (e: NumberFormatException) {
+            null
+        }
+    }
 
+    fun readFile() {
+       val currentFile = File(".")
+       val file = File(currentFile.absolutePath + "/a.txt")
+       val reader = BufferedReader(FileReader(file))
+       println(reader.readLine())
+       reader.close()
+    }
 
+    // Kotlin 에서는 try with resource 구문이 없습니다.
+    // 대신 Kotlin 의 언어적 특징을 활용해 close 를 호출해준다.
+    fun readFile(path: String) {
+        BufferedReader(FileReader(path)).use { br -> {
+            println(br.readLine())
+        }}
+    }
 }
