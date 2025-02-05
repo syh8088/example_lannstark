255f08c493c7523ce524368d88c22d0c15ca096b
syh8088 <syh8088@gmail.com>
2025-02-05 15:29:34 +0900

    6강. 코틀린에서 반복문을 다루는 방법
    6강. 코틀린에서 반복문을 다루는 방법

--- java-to-kotlin-starter-guide/src/main/java/com/lannstark/lec06/kotlin/lec06Main.kt
+++ java-to-kotlin-starter-guide/src/main/java/com/lannstark/lec06/kotlin/lec06Main.kt
@@ -2,7 +2,20 @@ package com.lannstark.lec06.kotlin
 
 fun main() {
 
+    val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    for (number in numbers) {
+        println(number)
+    }
 
+    for (i in 1..10) {
+        println(i)
+    }
 
+    for (i in 10 downTo 1) {
+        println(i)
+    }
 
+    for (i in 1..10 step 2) {
+        println(i)
+    }
 }
