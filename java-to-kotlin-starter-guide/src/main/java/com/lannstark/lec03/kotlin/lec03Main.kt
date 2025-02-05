87458d116dbe951c8e412440914aac73f212c544
syh8088 <syh8088@gmail.com>
2025-02-05 14:32:01 +0900

    3강. 코틀린에서 Type을 다루는 방법
    3강. 코틀린에서 Type을 다루는 방법

--- java-to-kotlin-starter-guide/src/main/java/com/lannstark/lec03/kotlin/lec03Main.kt
+++ java-to-kotlin-starter-guide/src/main/java/com/lannstark/lec03/kotlin/lec03Main.kt
@@ -1,8 +1,48 @@
 package com.lannstark.lec03.kotlin
 
+import com.lannstark.lec03.Person
+
 fun main() {
 
+    val number1 = 3
+    val number2: Long = number1.toLong()
+
+    val number3: Int? = 3
+    val number4: Long = number3?.toLong() ?: 0
+
+    val person = Person("test", 100)
+    println("이름 : ${person.name}")
+
+    var str = """
+        ABCD
+        EFG
+        ${person.name}
+    """.trimIndent()
+    println(str)
+
+    var str2 = "ABC"
+    println(str2[0])
+    println(str2[1])
+    println(str2[2])
 
 
+}
 
+fun printAgeIfPerson(obj: Any) {
+    if (obj is Person) {
+        val person = obj as Person
+        println(person.age)
+    }
+}
+
+fun printAgeIfPerson2(obj: Any) {
+    if (obj !is Person) {
+        val person = obj as Person
+        println(person.age)
+    }
+}
+
+fun printAgeIfPerson3(obj: Any?) {
+    var person = obj as? Person
+    println(person?.age)
 }
