224fbeb55d32c81f6c8f9c9704431881a2bfd6df
syh8088 <syh8088@gmail.com>
2025-02-15 16:51:24 +0900

    39강. Querydsl 사용하기 - 두 번째 방법
    39강. Querydsl 사용하기 - 두 번째 방법

--- Kotlin_Spring_Library_Application/library-app/src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
+++ Kotlin_Spring_Library_Application/library-app/src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
@@ -9,6 +9,7 @@ import com.group.libraryapp.dto.book.request.BookLoanRequest
 import com.group.libraryapp.dto.book.request.BookRequest
 import com.group.libraryapp.dto.book.request.BookReturnRequest
 import com.group.libraryapp.dto.book.response.BookStatResponse
+import com.group.libraryapp.repository.book.BookQuerydslRepository
 import com.group.libraryapp.util.fail
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -16,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class BookService(
     private val bookRepository: BookRepository,
+    private val bookQuerydslRepository: BookQuerydslRepository,
     private val userRepository: UserRepository,
     private val userLoanHistoryRepository: UserLoanHistoryRepository,
 ) {
@@ -90,7 +92,8 @@ class BookService(
     @Transactional(readOnly = true)
     fun getBookStatisticsV3(): List<BookStatResponse> {
 
-        return bookRepository.getStats()
+//        return bookRepository.getStats()
+        return bookQuerydslRepository.selectStats()
     }
 
 
