304e882d3373f5eb5cb70e6c423933ae7e1a287c
syh8088 <syh8088@gmail.com>
2025-02-16 22:40:21 +0900

    3강. 공변과 반공변
    3강. 공변과 반공변

--- advanced-kotlin/src/main/kotlin/generic/Cage2.kt
+++ advanced-kotlin/src/main/kotlin/generic/Cage2.kt
@@ -15,10 +15,16 @@ class Cage2<T : Any> {
     this.animals.add(animal)
   }
 
+  /**
+   * out: (함수 파라미터 입장에서의) 생성자, 공변
+   */
   fun moveFrom(otherCage: Cage2<out T>) {
     this.animals.addAll(otherCage.animals)
   }
 
+  /**
+   * int: (함수 파라미터 입장에서의) 소비자, 반공변
+   */
   fun moveTo(otherCage: Cage2<in T>) {
     otherCage.animals.addAll(this.animals)
   }
